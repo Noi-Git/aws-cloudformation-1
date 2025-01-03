ad75b5b294e80e588cc142d9c96f417175e3144f
Noi-Git <noi.patsin@gmail.com>
2025-01-02 19:55:30 -0500

    create an L3 constructor

--- lib/cdk-learning-stack.ts
+++ lib/cdk-learning-stack.ts
@@ -1,8 +1,12 @@
 import * as cdk from 'aws-cdk-lib'
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
+  }
+}
 export class CdkLearningStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
