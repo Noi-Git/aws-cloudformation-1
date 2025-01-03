71d59fd5566d202ada6086a713386707982119be
Noi-Git <noi.patsin@gmail.com>
2025-01-02 19:42:53 -0500

    use CDK to create L2 type bucket

--- lib/cdk-learning-stack.ts
+++ lib/cdk-learning-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkLearningStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLearningQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //use CDK construct to create AWS s3 bucket
+    // first way -- create an L2 bucket and set to expired in 2 days
+    new Bucket(this, 'NoiL2Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    })
   }
 }
