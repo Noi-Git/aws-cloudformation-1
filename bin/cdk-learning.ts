fdd1122a9c3468482791b4f4cc99a4df8e28926e
Noi-Git <noi.patsin@gmail.com>
2025-01-03 16:55:38 -0500

    provide our own name for s3 bucket

--- bin/cdk-learning.ts
+++ bin/cdk-learning.ts
@@ -4,4 +4,7 @@ import { PhotosStack } from '../lib/PhotosStack'
 import { Bucket } from 'aws-cdk-lib/aws-s3'
 
 const app = new cdk.App()
-new PhotosStack(app, 'PhotosStack', {})
+new Bucket(app, 'PhotosStack', {
+  // provide name for our bucket instead of have it randomly egnerated
+  bucketName: 'noi-photosbucket-slkdki',
+})
