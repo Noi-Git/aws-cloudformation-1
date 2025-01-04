60ac7e9f1617e3a1a2ac3b30ee6c1b7a313479e4
Noi-Git <noi.patsin@gmail.com>
2025-01-03 23:51:50 -0500

    create reusable function

--- bin/cdk-learning.ts
+++ bin/cdk-learning.ts
@@ -2,9 +2,16 @@
 import * as cdk from 'aws-cdk-lib'
 import { PhotosStack } from '../lib/PhotosStack'
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack'
+import { BucketTagger } from './Tagger'
 
 const app = new cdk.App()
 const photosStack = new PhotosStack(app, 'PhotosStack')
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
   targetBucketArn: photosStack.photosBucketArn,
 })
+
+// step 5: call the BucketTagger and add tag to all resources
+const tagger = new BucketTagger('level', 'test environment')
+
+// step 6: apply tagger to the whole application
+cdk.Aspects.of(app).add(tagger)
