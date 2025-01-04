f2cb73d52422804af1a487514290052489ffdc47
Noi-Git <noi.patsin@gmail.com>
2025-01-03 22:46:01 -0500

    add PhotosHandlerStack to the bin folder

--- bin/cdk-learning.ts
+++ bin/cdk-learning.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib'
 import { PhotosStack } from '../lib/PhotosStack'
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack'
 
 const app = new cdk.App()
 new PhotosStack(app, 'PhotosStack', {})
+new PhotosHandlerStack(app, 'PhotosHandlerStack')
