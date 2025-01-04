3124273130827e6fd4a1780b8a84ffe423283eba
Noi-Git <noi.patsin@gmail.com>
2025-01-03 22:32:34 -0500

    create reference to use in the PhotosHandlerStack

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -25,28 +25,26 @@ import { CfnOutput, Fn } from 'aws-cdk-lib'
 // }
 
 export class PhotosStack extends cdk.Stack {
-  // step 1: create a private suffix name
   private stackSuffix: string
   public readonly photosBucketArn: string
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // step 4: call the initializeSuffix function
     this.initializeSuffix()
 
-    // step 5: use the suffix created for physical id
     const photosBucket = new Bucket(this, 'PhotosBucket2', {
       bucketName: `noi-photo-bucket-${this.stackSuffix}`,
     })
-    this.photosBucketArn = photosBucket.bucketArn
-    // console.log('🚀 ~~:', Fn.select(2, Fn.split('/', this.stackId)))
+
+    // reference aws resources
+    new CfnOutput(this, 'noi-photo-bucket', {
+      value: photosBucket.bucketArn,
+      exportName: 'noi-photo-bucket',
+    })
   }
-  // step 2: initialize
   private initializeSuffix() {
-    // step 3: get the second index of the stack id
     const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
     this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId))
-    // console.log('🚀 ~ this.stackSuffix:', this.stackSuffix)
   }
 }
